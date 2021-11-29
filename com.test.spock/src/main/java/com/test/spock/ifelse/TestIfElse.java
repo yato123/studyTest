a769defc7519f8d5f4ef5eaf7941b18f7ec34ffd
陈子龙 <635317490@qq.com>
2021-11-29 22:31:19 +0800

    test

--- com.test.spock/src/main/java/com/test/spock/ifelse/TestIfElse.java
+++ com.test.spock/src/main/java/com/test/spock/ifelse/TestIfElse.java
@@ -2,6 +2,6 @@ package com.test.spock.ifelse;
 
 public class TestIfElse {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("124567");
     }
 }
